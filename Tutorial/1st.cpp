2bbddddc54ec413b43a15579c64245bc3576d7a9
Aditya <adityd2525@gmail.com>
2025-06-05 18:42:30 +0530

    Intial Commit

--- Tutorial/1st.cpp
+++ Tutorial/1st.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    cout << "Hello, Aditya Bhai!" << endl;
+    cout << "Hello, Aditya Bhai!\n";
     return 0;
 }
