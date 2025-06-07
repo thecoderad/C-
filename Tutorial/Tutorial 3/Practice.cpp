f0ff65918c3497d42f224974e15b2b0378089538
Aditya <adityd2525@gmail.com>
2025-06-07 22:02:27 +0530

    Tutorial 3

---
+++ Tutorial/Tutorial 3/Practice.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // This is single line comment
+    /* This is multiple lined comment in which you can test that it can infinite lines including 1 */
+    cout << "This is comment practice" << endl;
+}
