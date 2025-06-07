b4cba9bb4507ccff6c680fc5e998acccb038cab3
Aditya <adityd2525@gmail.com>
2025-06-07 22:14:14 +0530

    Intial Commit

--- Tutorial/Tutorial 4/4th.cpp
+++ Tutorial/Tutorial 4/4th.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
-}
+int main()
+{
+    /* Here we have an while statement which adi you have learnt so no time waste to see you requesting to telll you again!!! */
+    int count = 0;
+    while (count <= 10) {
+        count = count + 1;
+        cout << "Hello World!" << endl;
+        cout << count << endl; // This part is addition added by Adi
+    }
+
+    }
